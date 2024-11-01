b82cbc386f8eafa4c2ab89e81ec94f3e04746d93
Hamm Daniel <daniel.hamm@sanitas.com>
2024-11-01 17:54:44 +0100

    added some comments to the sqlite db

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import sqlite3
 
-from langgraph.checkpoint.sqlite import SqliteSaver
 from langchain_core.messages import HumanMessage, AIMessage
+from langgraph.checkpoint.sqlite import SqliteSaver
 
 from src.graph.full_graph import graph
 from src.langgraph_helper import graph_to_png
@@ -25,5 +25,24 @@ if not JUST_PRINT_GRAPH:
         events = agent_system.stream({"messages": ("user", user_input)}, config, stream_mode="values")
         for event in events:
             last_message = event["messages"][-1]
-            if isinstance(last_message, HumanMessage) or (isinstance(last_message, AIMessage) and not last_message.tool_calls):
+            if isinstance(last_message, HumanMessage) or (
+                    isinstance(last_message, AIMessage) and not last_message.tool_calls):
                 last_message.pretty_print()
+    # cursor = conn.cursor()
+    # cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+    # tables = cursor.fetchall()
+    # print("Tables:", tables)
+    #
+    # cursor.execute("PRAGMA table_info(checkpoints);")
+    # schema = cursor.fetchall()
+    # print("Schema:", schema)
+    #
+    # cursor.execute("PRAGMA table_info(writes);")
+    # schema = cursor.fetchall()
+    # print("Schema:", schema)
+    #
+    #
+    # cursor.execute("SELECT * FROM checkpoints")
+    # all_messages = cursor.fetchall()
+    # for msg in all_messages:
+    #     print(msg)  # Adjust based on actual column names and message format
